dabfd1c89013c0923f7d119a41d94bfbe9fa474c
albertinesilva-42 <albertinesilva_@hotmail.com>
2022-06-27 21:41:03 -0300

    Criação do Projeto Maven

---
+++ src/main/java/com/cadmototaxi/cadmototaxi/CadmototaxiApplication.java
@@ -0,0 +1,13 @@
+package com.cadmototaxi.cadmototaxi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadmototaxiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadmototaxiApplication.class, args);
+	}
+
+}
